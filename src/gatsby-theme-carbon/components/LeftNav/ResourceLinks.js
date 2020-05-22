51253dbc67fae8f76344cadfe9447b8468649bff
Hemankita Perabathini <Hemankita1@Hemankitas-MacBook-Pro-2.local>
2020-05-22 14:53:19 +0530

    adding org link

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -4,15 +4,11 @@ import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLi
 const links = [
   {
     title: 'Github',
-    href: 'https://github.com/carbon-design-system/gatsby-theme-carbon',
+    href: 'https://github.com/ibm-garage-ref-storefront',
   },
   {
-    title: 'Carbon',
-    href: 'https://www.carbondesignsystem.com',
-  },
-  {
-    title: 'Configuration guide',
-    href: '/guides/configuration',
+    title: 'Zenhub',
+    href: 'https://github.com/ibm-garage-ref-storefront/planning#zenhub',
   },
 ];
 
